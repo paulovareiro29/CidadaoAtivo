41461a277f2c9a07bb92864b70a7121fe4849e0e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 15:32:43 +0100

    Layout da atividade de criar nota pessoal.

--- app/src/main/java/ipvc/estg/cidadaoativo/CriarNotaPessoal.kt
+++ app/src/main/java/ipvc/estg/cidadaoativo/CriarNotaPessoal.kt
@@ -2,13 +2,30 @@ package ipvc.estg.cidadaoativo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import java.util.zip.Inflater
 
 class CriarNotaPessoal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTitle(R.string.criarNota)
         setContentView(R.layout.activity_criar_nota_pessoal)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_criar_notas_pessoais,menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.guardarNota -> {
+                //CRUD
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
